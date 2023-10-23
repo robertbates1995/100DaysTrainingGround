004472fd45c4fc8c8da3b4930953111d368bf987
Robert Bates <thegreatbobini@gmail.com>
2023-10-23 16:48:21 -0700

    creating WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Robert Bates on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
