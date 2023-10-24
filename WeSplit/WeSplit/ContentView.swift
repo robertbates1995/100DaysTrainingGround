cd85891eadd6fdd8088223899cf2990b2d330275
Robert Bates <thegreatbobini@gmail.com>
2023-10-24 10:33:06 -0700

    adding picker to WeSplit

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,16 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 1
     @State private var tipPercentage = 20
     
     let tipPercentages = [0, 10, 15, 20, 25]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad)
-            }
+        NavigationStack{
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("number of people", selection: $numberOfPeople) {
+                        ForEach(1..<100) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+            }.navigationTitle("WeSplit")
         }
     }
 }
