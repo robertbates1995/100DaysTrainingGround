c006bc1c00a6cd87cbe25d853e4d7cb3aec4e591
Robert Bates <thegreatbobini@gmail.com>
2023-10-27 16:37:47 -0700

    finished temperature converting challenge project

--- FirstChallangeProject/FirstChallangeProject/ContentView.swift
+++ FirstChallangeProject/FirstChallangeProject/ContentView.swift
@@ -9,16 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var startingUnit = "Celcius"
+    @State private var startingUnit = "°C"
+    @State private var endingUnit = "°C"
+    @State private var startingValue = 0.0
+    @FocusState private var valueIsFocused: Bool
     
-    let units: [String] = ["°C", "°F", "K"]
+    var endingValue: Double {
+        if(startingUnit == endingUnit) {
+            return startingValue
+        }
+        if(startingUnit == "°C") {
+            let celsius = Measurement(value: startingValue, unit: UnitTemperature.celsius)
+            if(endingUnit == "°F") {
+                return celsius.converted(to: .fahrenheit).value
+            }
+            return celsius.converted(to: .kelvin).value
+        }
+        if(startingUnit == "°F") {
+            let fahrenheit = Measurement(value: startingValue, unit: UnitTemperature.fahrenheit)
+            if(endingUnit == "°C") {
+                return fahrenheit.converted(to: .celsius).value
+            }
+            return fahrenheit.converted(to: .kelvin).value
+        }
+        if(startingUnit == "K") {
+            let kelven = Measurement(value: startingValue, unit: UnitTemperature.kelvin)
+            if(endingUnit == "°C") {
+                return kelven.converted(to: .celsius).value
+            }
+            return kelven.converted(to: .fahrenheit).value
+        }
+        return startingValue
+    }
+    
+    let units = ["°C", "°F", "K"]
     
 
     var body: some View {
         NavigationStack {
             Form {
-                Section("starting Unit") {
-                    Picker("starting Unit", selection: $startingUnit) {
+                Section("Starting value") {
+                    TextField("inital value", value: $startingValue, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($valueIsFocused)
+                    Picker("Starting Unit", selection: $startingUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                Section("Ending value") {
+                    Text(endingValue.description)
+                    Picker("Ending Unit", selection: $endingUnit) {
                         ForEach(units, id: \.self) {
                             Text($0)
                         }
@@ -26,6 +68,13 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Temperature Converter")
+            .toolbar{
+                if valueIsFocused {
+                    Button("Done") {
+                        valueIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
