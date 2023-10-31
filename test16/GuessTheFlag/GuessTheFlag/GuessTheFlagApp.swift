4ac08d83757ed30e79646ac02c292334678aefd7
Robert Bates <thegreatbobini@gmail.com>
2023-10-30 17:17:34 -0700

    added guess the flag

---
+++ test16/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Robert Bates on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
