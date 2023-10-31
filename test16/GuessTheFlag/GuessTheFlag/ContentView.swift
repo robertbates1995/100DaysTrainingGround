faac1e39cd0bd160aa5ff75789580c626649ffc9
Robert Bates <thegreatbobini@gmail.com>
2023-10-30 18:14:59 -0700

    learning nuances with buttons, labels, and alerts

--- test16/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ test16/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
                 Color.red.ignoresSafeArea()
-                Color.blue.ignoresSafeArea()
+                Color.yellow.ignoresSafeArea()
             }
-            Text("Hello, world!")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
+            Button {
+                print("button was tapped")
+                showingAlert = true
+            } label: {
+                Label("test button", systemImage: "pencil")
+                    .foregroundStyle(.secondary)
+                    .padding(50)
+                    .background(.ultraThinMaterial)
+            }
+            .alert("Important message", isPresented: $showingAlert) {
+                Button("Delete", role: .destructive) { }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("this is a test message")
+            }
         }
     }
 }
