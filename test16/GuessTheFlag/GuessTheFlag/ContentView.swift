b2562a24c89f5d9bdba33c969208666bbb8bff23
Robert Bates <thegreatbobini@gmail.com>
2023-10-30 17:37:53 -0700

    experimenting with zstack

--- test16/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ test16/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        ZStack {
+            VStack{
+                Color.secondary.ignoresSafeArea()
+            }
             Text("Hello, world!")
         }
-        .padding()
     }
 }
 
