c84e97b073c82a49aa776cb37e40f8366e0bd2f7
Robert Bates <thegreatbobini@gmail.com>
2023-11-10 15:46:39 -0800

    improved flag visuals. added corner rounding

--- test16/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ test16/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -17,26 +17,37 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
-            VStack(spacing: 30) {
-                Text("Tap the flag of")
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            VStack(spacing: 30){
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
                     .foregroundStyle(.white)
-                    .font(.subheadline.weight(.heavy))
-                Text(countries[correctAnswer])
-                    .foregroundStyle(.white)
-                    .font(.subheadline.weight(.semibold))
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
+                VStack(spacing: 15 ) {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                        .font(.subheadline.weight(.semibold))
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.rect(cornerRadius: 10))
+                                .shadow(radius: 50)
+                        }
                     }
                 }
-            }
-            .alert(scoreTitle, isPresented: $showingScore) {
-                Button("Continue", action: askQuestion)
-            } message: {
-                Text("Your score is: ???")
+                .alert(scoreTitle, isPresented: $showingScore) {
+                    Button("Continue", action: askQuestion)
+                } message: {
+                    Text("Your score is: ???")
+                }
             }
         }
     }
