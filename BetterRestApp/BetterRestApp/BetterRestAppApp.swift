518cbb054aa173a79e70ccdca90e417155ff602b
Robert Bates <thegreatbobini@gmail.com>
2023-12-30 17:14:21 -0800

    creating better rest

---
+++ BetterRestApp/BetterRestApp/BetterRestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestAppApp.swift
+//  BetterRestApp
+//
+//  Created by Robert Bates on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
