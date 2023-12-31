3097dd8df650eaaf6bc7839800eaed7ff01b0349
Robert Bates <thegreatbobini@gmail.com>
2023-12-30 18:49:36 -0800

    playing around with displaying dates

--- BetterRestApp/BetterRestApp/ContentView.swift
+++ BetterRestApp/BetterRestApp/ContentView.swift
@@ -10,14 +10,20 @@ import SwiftUI
 @Observable
 class ContentModel {
     var sleepAmount = 8.0
-    
+    var wakeUp = Date.now
+    var now = Date.now
+    var hour = Calendar.current.dateComponents([.hour], from: .now)
+    var minute = Calendar.current.dateComponents([.minute], from: .now)
+    private var components: DateComponents {
+        Calendar.current.dateComponents([.hour, .minute], from: .now)
+    }
 }
 
 struct ContentView: View {
     @State private var model = ContentModel()
     
     var body: some View {
-        Stepper("\(model.sleepAmount.formatted())", value: $model.sleepAmount, in: 4...12, step: 0.25)
+        DatePicker("Please enter a date", selection: $model.wakeUp, displayedComponents: .hourAndMinute).labelsHidden().padding()
     }
 }
 
