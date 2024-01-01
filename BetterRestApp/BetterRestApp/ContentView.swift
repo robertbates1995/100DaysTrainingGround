2220cd38620ebeab114f0115f02f197df6b41a3f
Robert Bates <thegreatbobini@gmail.com>
2024-01-01 15:22:25 -0800

    working on BetterRest

--- BetterRestApp/BetterRestApp/ContentView.swift
+++ BetterRestApp/BetterRestApp/ContentView.swift
@@ -5,16 +5,38 @@
 //  Created by Robert Bates on 12/30/23.
 //
 
+import CoreML
 import SwiftUI
 
 @Observable
 class ContentModel {
+    var wakeUp = Date.now
     var sleepAmount = 8.0
     var coffeeAmount = 1
-    var wakeUp = Date.now
+    let sleepRange: ClosedRange<Double> = 4...12
+    let sleepStepValue = 0.25
+    let coffeeRange = 0...20
+    
+    var alertTitle = ""
+    var alertMessage = ""
+    var showingAlert = false
     
     func calculateBedtime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediciton = try model.prediction(wake: Int64((hour + minute)), estimatedSleep: sleepAmount, coffee: Int64(coffeeAmount))
+            let bedTime = wakeUp - prediciton.actualSleep
+            alertTitle = "Your bedtime is..."
+            alertMessage = bedTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "There was an issue calculating your bedtime."
+        }
+        showingAlert = true
     }
 }
 
@@ -30,15 +52,20 @@ struct ContentView: View {
                     .labelsHidden()
                 Text("Desired amount of sleep")
                     .font(.headline)
-                Stepper("\(model.sleepAmount.formatted()) hours", value: $model.sleepAmount, in: 4...12, step: 0.25)
+                Stepper("\(model.sleepAmount.formatted()) hours", value: $model.sleepAmount, in: model.sleepRange, step: model.sleepStepValue)
                 Text("Daily coffee intake")
                     .font(.headline)
-                Stepper("\(model.coffeeAmount) cup(s)", value: $model.coffeeAmount, in: 1...20)
+                Stepper("\(model.coffeeAmount) cup(s)", value: $model.coffeeAmount, in: model.coffeeRange)
             }.padding()
             .navigationTitle("BetterRest")
-                .toolbar {
-                    Button("Calculate", action: model.calculateBedtime)
-                }
+            .toolbar {
+                Button("Calculate", action: model.calculateBedtime)
+            }
+            .alert(model.alertTitle, isPresented: $model.showingAlert) {
+                Button("OK") { }
+            }message: {
+                Text(model.alertMessage)
+            }
         }
     }
 }
