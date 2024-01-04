726960c8274b8f4e212d4d1c6d1c7e40e7fed065
Robert Bates <thegreatbobini@gmail.com>
2024-01-04 13:42:57 -0800

    working with lists

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+
+            Section("Section 3") {
+                Text("Static row 3")
+                Text("Static row 4")
+            }
         }
-        .padding()
     }
 }
 
