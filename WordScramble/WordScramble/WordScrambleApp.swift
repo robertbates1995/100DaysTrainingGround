7a8c3b3f3a695e8232f386f5b73ee2965320efba
Robert Bates <thegreatbobini@gmail.com>
2024-01-04 13:38:53 -0800

    created WordScramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Robert Bates on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
