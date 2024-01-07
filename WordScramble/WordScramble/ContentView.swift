63810f9df010b2510bbc4ac0dbf63af1ca408785
Robert Bates <thegreatbobini@gmail.com>
2024-01-07 13:34:49 -0800

    added testString function

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -14,13 +14,11 @@ struct ContentView: View {
                 Text("Static row 1")
                 Text("Static row 2")
             }
-            
             Section("Section 2") {
                 ForEach(0..<5) {
                     Text("Dynamic row \($0)")
                 }
             }
-            
             Section("Section 3") {
                 Text("Static row 3")
                 Text("Static row 4")
@@ -34,6 +32,18 @@ struct ContentView: View {
                 
             }
         }
+    }
+    
+    func testString() {
+        let sut = "a\nb\nc"
+        let letters = sut.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
     }
 }
 
