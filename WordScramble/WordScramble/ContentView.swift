e01bea8ea18c92f6f4da1884fdee7e29eb2ce4e9
Robert Bates <thegreatbobini@gmail.com>
2024-01-07 13:44:31 -0800

    created and added to viewModel

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -7,24 +7,10 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        List {
-            Section("Section 1") {
-                Text("Static row 1")
-                Text("Static row 2")
-            }
-            Section("Section 2") {
-                ForEach(0..<5) {
-                    Text("Dynamic row \($0)")
-                }
-            }
-            Section("Section 3") {
-                Text("Static row 3")
-                Text("Static row 4")
-            }
-        }
-    }
+class ContentModel {
+    var usedWords = [String]()
+    var rootWord = ""
+    var newWord = ""
     
     func testBundles() {
         if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
@@ -44,6 +30,18 @@ struct ContentView: View {
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         let allGood = misspelledRange.location == NSNotFound
+    }
+}
+
+struct ContentView: View {
+    @State var model = ContentModel()
+    
+    var body: some View {
+        List {
+            Section {
+                TextField("Enter your word", text: $model.newWord)
+            }
+        }
     }
 }
 
