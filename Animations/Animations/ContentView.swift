f1c5ae203844677d17d8dcc364a6888837ab4e85
Robert Bates <thegreatbobini@gmail.com>
2024-01-10 15:03:06 -0800

    adding custom animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -16,35 +16,16 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        VStack {
-            Stepper(
-                "Scale amount",
-                value: $animationAmount.animation(
-                    .easeInOut(duration: 0.3)
-                    .repeatCount(3, autoreverses: true)
-                ),
-                in: 1...10
-            )
-            
-            Spacer()
-            
-            Button("Tap me") {
-                animationAmount += 1
+        Button("Tap me") {
+            withAnimation {
+                animationAmount += 360
             }
-            .padding(40)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .scaleEffect(animationAmount)
-            
-            Button("test") {
-                
-            }
-            .padding(40)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
         }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 1))
     }
 }
 
