2a7ea2c11cf3397a40adeed5817618cd985e4ac8
Robert Bates <thegreatbobini@gmail.com>
2024-01-11 13:33:39 -0800

    building custom transitions

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -16,22 +16,45 @@ class ContentModel {
     var letters = Array("Hello SwiftUI")
 }
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading), 
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
 struct ContentView: View {
     @Bindable var model = ContentModel()
     
     var body: some View {
-        VStack {
-            Button("Tap me") {
-                withAnimation {
-                    model.isShowingRed.toggle()
-                }
-            }
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
             
             if model.isShowingRed {
                 Rectangle()
                     .fill(.red)
                     .frame(width: 200, height: 200)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                model.isShowingRed.toggle()
             }
         }
     }
