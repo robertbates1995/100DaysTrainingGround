cd1eb2ad2ba45bdefe9854ef780ee9e4989305cc
Robert Bates <thegreatbobini@gmail.com>
2024-01-13 18:00:27 -0800

    creating edutainment app

---
+++ EdutainmentGame/EdutainmentGame/EdutainmentGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentGameApp.swift
+//  EdutainmentGame
+//
+//  Created by Robert Bates on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
