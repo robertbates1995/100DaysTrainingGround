84b53c6ea1606a89d5f58b1d478b49ea444d787c
Robert Bates <thegreatbobini@gmail.com>
2024-01-18 11:26:23 -0800

    populating questions in model

--- EdutainmentGame/EdutainmentGame/ContentView.swift
+++ EdutainmentGame/EdutainmentGame/ContentView.swift
@@ -15,15 +15,25 @@ class ContentModel {
     var numberOfQuestions = 5
     var upperLimit = 10
     var upperLimitRange = (2...12)
-    var gameState = gameState.playing
+    var gameState = gameState.setup
+    var userAnswer = 0
     var score = 0
+    var questions: [(Int, Int, Int)] = []
     
     enum gameState {
         case setup, playing, over
     }
     
     func startGame() {
-        
+        var temp = self.numberOfQuestions
+        while temp > 0 {
+            let temp1 = Int.random(in: 1...upperLimit)
+            let temp2 = Int.random(in: 1...upperLimit)
+            let answer = temp1 * temp2
+            questions += [(temp1, temp2, answer)]
+            temp -= 1
+        }
+        self.gameState = .playing
     }
 }
 
@@ -53,25 +63,25 @@ struct ContentView: View {
                 }.pickerStyle(.segmented)
             }
             Section("Upper Limit") {
-                Stepper("\(model.upperLimit)", value: $model.upperLimit, in: model.upperLimitRange) { _ in
-                    
-                }
+                Stepper("\(model.upperLimit)", value: $model.upperLimit, in: model.upperLimitRange)
             }
             Section("Start Game") {
                 Button("Start") {
-                    model.gameState = .playing
+                    model.startGame()
                 }
             }
         }.navigationTitle("Starting Screen")
     }
     
     func gameView() -> some View {
-        if model.numberOfQuestions > 0 {
+        if model.numberOfQuestions < 1 {
             model.gameState = .over
         }
+        
+        
         return List {
-            Section("current Question") {
-                    
+            Section("what is ") {
+                TextField("test field", value: $model.userAnswer, format: .number)
             }
         }.navigationTitle("Game Screen")
     }
