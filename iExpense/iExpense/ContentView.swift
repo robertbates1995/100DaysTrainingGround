a5fb005f545315c023124631d2b549a475abed1d
Robert Bates <thegreatbobini@gmail.com>
2024-01-22 19:55:01 -0800

    creating iExpense

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Robert Bates on 1/22/24.
+//
+
+import SwiftUI
+
+@Observable
+class user {
+    
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Robert Bates on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
