2c4e85689ff5a2082d0077783bdfc40e2218b2c6
Robert Bates <thegreatbobini@gmail.com>
2024-02-04 20:13:17 -0800

    creating Moonshot

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Robert Bates on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
