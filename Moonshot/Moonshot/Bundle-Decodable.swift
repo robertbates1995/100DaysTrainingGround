33b5d47695fd5d44235a872dda0925308299b989
Robert Bates <thegreatbobini@gmail.com>
2024-02-17 17:28:57 -0800

    ignoring pictures and adding files

---
+++ Moonshot/Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,28 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Robert Bates on 2/17/24.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) in bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
+    }
+}
