80c0646fc27f47ae354a092d43f1a3c169069293
Robert Bates <thegreatbobini@gmail.com>
2024-02-17 17:19:01 -0800

    adding to Moonshot

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Robert Bates on 2/17/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
