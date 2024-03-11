cb11a78a317a26717464fba9009a41222154f62d
Robert Bates <thegreatbobini@gmail.com>
2024-03-11 12:50:00 -0700

    creating new navigation app project

---
+++ NavigationApp/NavigationApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationApp
+//
+//  Created by Robert Bates on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationApp/NavigationApp/NavigationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationAppApp.swift
+//  NavigationApp
+//
+//  Created by Robert Bates on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
