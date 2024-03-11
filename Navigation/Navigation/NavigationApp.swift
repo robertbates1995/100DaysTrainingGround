ea0d964a6ac604d208aef1220fdddcd6a1e6c0d3
Robert Bates <thegreatbobini@gmail.com>
2024-03-11 12:54:09 -0700

    creating navigation app

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Robert Bates on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
