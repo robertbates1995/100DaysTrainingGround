350b40b0d6a47ac061dfdb48d5d917171486699d
Robert Bates <thegreatbobini@gmail.com>
2024-03-18 14:45:24 -0700

    navigation app updates

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -8,34 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var path = NavigationPath()
+    @State private var path = [Int]()
     
     var body: some View {
-        NavigationStack(path: $path) {
-            List {
-                ForEach(0..<5) { i in
-                    NavigationLink("Select Number: \(i)", value: i)
-                }
-                
-                ForEach(0..<5) { i in
-                    NavigationLink("Select String: \(i)", value: String(i))
-                }
+        NavigationStack {
+            List(0..<100) { i in
+                Text("row \(i)")
             }
-            .toolbar {
-                Button("Push 556") {
-                    path.append(556)
-                }
-                
-                Button("Push Hello") {
-                    path.append("Hello")
-                }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected the number \(selection)")
-            }
-            .navigationDestination(for: String.self) { selection in
-                Text("You selected the string \(selection)")
-            }
+            .navigationTitle("title goes here")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.blue)
+            .toolbarColorScheme(.dark)
+            .toolbar(.hidden, for: .navigationBar)
         }
     }
 }
