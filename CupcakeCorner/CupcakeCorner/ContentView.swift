845e7809e6865566c8427eecae552ceda3374767
Robert Bates <thegreatbobini@gmail.com>
2024-03-21 16:24:54 -0700

    validating and disabling forms

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,20 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool { username.count < 5 || email.count < 5 }
     
     var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("there was an error loading the image.")
-            } else {
-                ProgressView()
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
+            Section {
+                Button("Create Account") {
+                    print("Creating account...")
+                }
+            }
+            .disabled(username.isEmpty || email.isEmpty)
         }
-        .frame(width: 200, height: 200)
     }
 }
 
