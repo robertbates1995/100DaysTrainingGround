485f553478443b7ad1e3de2f41c3b16994db1ca3
Robert Bates <thegreatbobini@gmail.com>
2024-03-21 15:35:11 -0700

    creating Cupcake Corner app

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Robert Bates on 3/21/24.
+//
+
+import SwiftUI
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Robert Bates on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
