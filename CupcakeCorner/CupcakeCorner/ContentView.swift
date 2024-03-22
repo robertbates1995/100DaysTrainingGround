08e63ac2e4147c4e8d20da9f845f20299c178b7b
Robert Bates <thegreatbobini@gmail.com>
2024-03-22 14:53:16 -0700

    partitioning order into it's own class

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,37 +7,6 @@
 
 import SwiftUI
 
-@Observable
-class Order {
-    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
-    
-    var type = 0
-    var quantity = 6
-    var name = ""
-    var streetAddress = ""
-    var city = ""
-    var zip = ""
-    var extraFrosting = false
-    var addSprinkles = false
-    
-    var specialRequestEnabled = false {
-        didSet {
-            if specialRequestEnabled == false {
-                extraFrosting = false
-                addSprinkles = false
-            }
-        }
-    }
-    
-    var hasValidAddress: Bool {
-        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
-            return false
-        }
-        
-        return true
-    }
-}
-
 struct ContentView: View {
     @State private var order = Order()
     
---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,39 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Robert Bates on 3/22/24.
+//
+
+import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 6
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    var extraFrosting = false
+    var addSprinkles = false
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        
+        return true
+    }
+}
