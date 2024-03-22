ec4e58ca6e3dfd0c537fe2af6f7e54180d31188d
Robert Bates <thegreatbobini@gmail.com>
2024-03-22 13:58:33 -0700

    Starting Cupcake Corner views

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Robert Bates on 3/22/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
