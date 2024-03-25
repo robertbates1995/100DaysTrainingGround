52862dc567ea9b003e8231bb8c2fdc12f5c0a33a
Robert Bates <thegreatbobini@gmail.com>
2024-03-25 13:25:43 -0700

    created toggle push button

--- BookWorm/BookWorm/ContentView.swift
+++ BookWorm/BookWorm/ContentView.swift
@@ -7,11 +7,30 @@
 
 import SwiftUI
 
+struct PushButton: View {
+    let title: String
+    @State var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.2)]
+    
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
 struct ContentView: View {
     @State private var rememberMe = false
     
     var body: some View {
-        Toggle("Remember Me", isOn: $rememberMe)
+        PushButton(title: "Test", isOn: false)
     }
 }
 
