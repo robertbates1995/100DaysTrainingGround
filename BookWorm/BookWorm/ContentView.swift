71ac41f9e66c0f268078369ef4c4517d7e7a5987
Robert Bates <thegreatbobini@gmail.com>
2024-03-25 13:45:22 -0700

    using app storage to allow for long notes

--- BookWorm/BookWorm/ContentView.swift
+++ BookWorm/BookWorm/ContentView.swift
@@ -7,30 +7,15 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-    @State var isOn: Bool
-    
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.2)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
-        .foregroundStyle(.white)
-        .clipShape(.capsule)
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
-
 struct ContentView: View {
-    @State private var rememberMe = false
+    @AppStorage("notes") private var notes = ""
     
     var body: some View {
-        PushButton(title: "Test", isOn: false)
+        NavigationStack {
+            TextEditor(text: $notes)
+                .navigationTitle("Notes")
+                .padding()
+        }
     }
 }
 
