885c4c24ebcfdfba1c612e65166ade0ee9bf4c9a
Robert Bates <thegreatbobini@gmail.com>
2024-04-01 19:15:47 -0700

    added preview and implemented view

---
+++ BookWorm/BookWorm/DetailView.swift
@@ -0,0 +1,48 @@
+//
+//  DetailView.swift
+//  BookWorm
+//
+//  Created by Robert Bates on 4/1/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+
+                Text(book.genre.uppercased())
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        let example = Book(title: "Test Book", author: "Test Author", genre: "Fantasy", review: "Test review", rating: 4)
+        
+        return DetailView(book: example)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create preview: \(error.localizedDescription)")
+    }
+}
