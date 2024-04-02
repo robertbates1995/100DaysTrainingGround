287b6a35c8dbba656103a45025e6ead0f6bb0955
Robert Bates <thegreatbobini@gmail.com>
2024-04-01 19:19:32 -0700

    adding to detailView

--- BookWorm/BookWorm/DetailView.swift
+++ BookWorm/BookWorm/DetailView.swift
@@ -27,6 +27,15 @@ struct DetailView: View {
                     .clipShape(.capsule)
                     .offset(x: -5, y: -5)
             }
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+
+            Text(book.review)
+                .padding()
+
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
         }
         .navigationTitle(book.title)
         .navigationBarTitleDisplayMode(.inline)
