2ad4f06037e21a7e83c41656c9136f5355623b6f
Robert Bates <thegreatbobini@gmail.com>
2024-04-03 17:32:33 -0700

    creating SwftDataProject

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Robert Bates on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
