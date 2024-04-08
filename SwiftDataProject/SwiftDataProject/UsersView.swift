5067ea8c35dd66161c5668db855b3ba9500f283f
Robert Bates <thegreatbobini@gmail.com>
2024-04-07 17:19:44 -0700

    adding test users to model

--- SwiftDataProject/SwiftDataProject/UsersView.swift
+++ SwiftDataProject/SwiftDataProject/UsersView.swift
@@ -5,15 +5,28 @@
 //  Created by Robert Bates on 4/5/24.
 //
 
-import SwiftData
 import SwiftUI
+import SwiftData
 
 struct UsersView: View {
+    @Environment(\.modelContext) var modelContext
     @Query var users: [User]
     
     var body: some View {
         List(users) { user in
-            Text(user.name)
+            HStack{
+                Text(user.name)
+                
+                Spacer()
+                
+                Text(String(user.jobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
         }
     }
     
@@ -21,6 +34,17 @@ struct UsersView: View {
         _users = Query(filter: #Predicate<User> { user in
             user.joinDate >= minimumJoinDate
         }, sort: sortOrder)
+    }
+    
+    func addSample() {
+        let user1 = User(name: "Test Name1", city: "Test City1", joinDate: .now)
+        let job1 = Job(name: "Job 1", priority: 1)
+        let job2 = Job(name: "Job 2", priority: 2)
+        
+        modelContext.insert(user1)
+        
+        user1.jobs.append(job1)
+        user1.jobs.append(job2)
     }
 }
 
