791d916a2f3cc30898a8023bd2fb8c3b0f13fee0
Robert Bates <thegreatbobini@gmail.com>
2024-04-24 12:13:54 -0700

    starting to add SwiftData conformance

--- Mile60/Mile60/Models.swift
+++ Mile60/Mile60/Models.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import SwiftData
 
-struct User: Codable, Identifiable {
+class User: Codable, Identifiable {
     var id: String
     var isActive: Bool
     var name: String
@@ -19,11 +20,48 @@ struct User: Codable, Identifiable {
     var registered: String
     var tags: [String]
     var friends: [Friend]
+    
+    init(id: String, isActive: Bool, name: String, age: Int, company: String, email: String, address: String, about: String, registered: String, tags: [String], friends: [Friend]) {
+        self.id = id
+        self.isActive = isActive
+        self.name = name
+        self.age = age
+        self.company = company
+        self.email = email
+        self.address = address
+        self.about = about
+        self.registered = registered
+        self.tags = tags
+        self.friends = friends
+    }
 }
 
-struct Friend: Codable {
+@Model
+class Friend: Codable {
+    enum CodingKeys: String, CodingKey {
+        case id = "friendId"
+        case name = "friendName"
+    }
+    
     var id: String
     var name: String
+    
+    init(id: String, name: String) {
+        self.id = id
+        self.name = name
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.name, forKey: .name)
+        try container.encode(self.id, forKey: .id)
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+    }
 }
 
 
