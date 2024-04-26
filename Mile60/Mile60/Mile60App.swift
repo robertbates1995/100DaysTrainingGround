6418edebef39f91a39a4a4a9731da81233ddbf4b
Robert Bates <thegreatbobini@gmail.com>
2024-04-25 20:42:30 -0700

    adding Codable conformance

--- Mile60/Mile60/Mile60App.swift
+++ Mile60/Mile60/Mile60App.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct Mile60App: App {
@@ -13,5 +14,6 @@ struct Mile60App: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
--- Mile60/Mile60/Models.swift
+++ Mile60/Mile60/Models.swift
@@ -8,7 +8,22 @@
 import Foundation
 import SwiftData
 
+@Model
 class User: Codable, Identifiable {
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case isActive = "isActive"
+        case name = "name"
+        case age = "age"
+        case company = "company"
+        case email = "email"
+        case address = "address"
+        case about = "about"
+        case registered = "registered"
+        case tags = "tags"
+        case friends = "friends"
+    }
     var id: String
     var isActive: Bool
     var name: String
@@ -34,13 +49,43 @@ class User: Codable, Identifiable {
         self.tags = tags
         self.friends = friends
     }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.id, forKey: .id)
+        try container.encode(self.isActive, forKey: .isActive)
+        try container.encode(self.name, forKey: .name)
+        try container.encode(self.age, forKey: .age)
+        try container.encode(self.company, forKey: .company)
+        try container.encode(self.email, forKey: .email)
+        try container.encode(self.address, forKey: .address)
+        try container.encode(self.about, forKey: .about)
+        try container.encode(self.registered, forKey: .registered)
+        try container.encode(self.tags, forKey: .tags)
+        try container.encode(self.friends, forKey: .friends)
+    }
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.isActive = try container.decode(Bool.self, forKey: .isActive)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.age = try container.decode(Int.self, forKey: .age)
+        self.company = try container.decode(String.self, forKey: .company)
+        self.email = try container.decode(String.self, forKey: .email)
+        self.address = try container.decode(String.self, forKey: .address)
+        self.about = try container.decode(String.self, forKey: .about)
+        self.registered = try container.decode(String.self, forKey: .registered)
+        self.tags = try container.decode([String].self, forKey: .tags)
+        self.friends = try container.decode([Friend].self, forKey: .friends)
+    }
 }
 
 @Model
 class Friend: Codable {
     enum CodingKeys: String, CodingKey {
-        case id = "friendId"
-        case name = "friendName"
+        case id = "id"
+        case name = "name"
     }
     
     var id: String
