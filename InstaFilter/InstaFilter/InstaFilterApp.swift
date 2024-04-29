077730f4eed2e587158cafca61f55551e25a8f82
Robert Bates <thegreatbobini@gmail.com>
2024-04-29 13:41:23 -0700

    creating InstaFilter app

---
+++ InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Robert Bates on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
