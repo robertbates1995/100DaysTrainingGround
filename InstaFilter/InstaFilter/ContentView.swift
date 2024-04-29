eb8be87e61a6489caf5d1b8a921ee72760c9e854
Robert Bates <thegreatbobini@gmail.com>
2024-04-29 14:22:16 -0700

    using .onChange to print slider value

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -8,19 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0 {
-        didSet {
-            print("New value is \(blurAmount)")
-        }
-    }
-
+    @State private var blurAmount = 0.0
+    
     var body: some View {
         VStack {
             Text("Hello, World!")
                 .blur(radius: blurAmount)
-
+            
             Slider(value: $blurAmount, in: 0...20)
-
+                .onChange(of: blurAmount) { oldValue, newValue in
+                    print("New value is \(newValue)")
+                }
+            
             Button("Random Blur") {
                 blurAmount = Double.random(in: 0...20)
             }
