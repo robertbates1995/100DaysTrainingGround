b4237a8245659d6882a05cbdadf0a619ba306e80
Robert Bates <thegreatbobini@gmail.com>
2024-04-30 18:18:48 -0700

    using conditionals and KCInput

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -27,10 +27,16 @@ struct ContentView: View {
         
         let context = CIContext()
         let currentFilter = CIFilter.twirlDistortion()
-        
         currentFilter.inputImage = beginImage
-        currentFilter.radius = 300
-        currentFilter.center = CGPoint(x: inputImage.size.width/2, y: inputImage.size.height/2)
+
+        let amount = 1.0
+
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
         
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
