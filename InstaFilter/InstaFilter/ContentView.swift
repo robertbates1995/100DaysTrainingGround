a3c13c8b57ef2c221baf41fd84c7ce07d5eca904
Robert Bates <thegreatbobini@gmail.com>
2024-04-30 18:15:06 -0700

    adding filters to CGImage

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -5,28 +5,41 @@
 //  Created by Robert Bates on 4/29/24.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
-
+    @State private var image: Image?
+    
     var body: some View {
-        Button("Hello, World!") {
-            showingConfirmation = true
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .frame(width: 300, height: 300)
-        .background(backgroundColor)
-        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-            Button("Red") { backgroundColor = .red }
-            Button("Green") { backgroundColor = .green }
-            Button("Blue") { backgroundColor = .blue }
-            Button("Cancel", role: .cancel) { }
-        } message: {
-            Text("Select a new color")
-        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        let inputImage = UIImage(resource: .paper)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        
+        currentFilter.inputImage = beginImage
+        currentFilter.radius = 300
+        currentFilter.center = CGPoint(x: inputImage.size.width/2, y: inputImage.size.height/2)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
     }
 }
+
 #Preview {
     ContentView()
 }
