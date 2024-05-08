62ac67bc4fb197450766f0e94c4529e86a846c5e
Robert Bates <thegreatbobini@gmail.com>
2024-05-08 15:03:10 -0700

    incrementing filter count

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -8,6 +8,7 @@
 import CoreImage
 import CoreImage.CIFilterBuiltins
 import PhotosUI
+import StoreKit
 import SwiftUI
 
 struct ContentView: View {
@@ -17,6 +18,7 @@ struct ContentView: View {
     @State private var showingFilters = false
     
     @AppStorage("filterCount") var filterCount = 0
+    @Environment(\.requestReview) var requestReview
     
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     let context = CIContext()
@@ -102,9 +104,15 @@ struct ContentView: View {
         processedImage = Image(uiImage: uiImage)
     }
     
-    func setFilter(_ filter: CIFilter) {
+    @MainActor func setFilter(_ filter: CIFilter) {
         currentFilter = filter
         loadImage()
+        
+        filterCount += 1
+        
+        if filterCount >= 20 {
+            requestReview()
+        }
     }
 }
 
