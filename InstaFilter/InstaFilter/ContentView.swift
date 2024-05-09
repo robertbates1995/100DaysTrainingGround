06820b5de12b24901b3d23552f80e4602a2f9056
Robert Bates <thegreatbobini@gmail.com>
2024-05-08 17:00:21 -0700

    working on challenge problems

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
     @AppStorage("filterCount") var filterCount = 0
     @Environment(\.requestReview) var requestReview
     
-    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var currentFilters: [CIFilter] = [CIFilter.sepiaTone()]
     let context = CIContext()
     
     var body: some View {
@@ -38,21 +38,28 @@ struct ContentView: View {
                 }
                 .buttonStyle(.plain)
                 .onChange(of: selectedItem, loadImage)
-
+                
                 Spacer()
-
+                
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
                         .onChange(of: filterIntensity, applyProcessing)
                 }
                 .padding(.vertical)
-
+                
                 HStack {
-                    Button("Change Filter") {
-                        changeFilter()
+                    if processedImage == nil {
+                        Button("Change Filter") {
+                            changeFilter()
+                        }
+                        .disabled(true)
+                    } else {
+                        Button("Change Filter") {
+                            changeFilter()
+                        }
                     }
-
+                    
                     Spacer()
                     
                     if let processedImage {
@@ -85,18 +92,22 @@ struct ContentView: View {
             guard let inputImage = UIImage(data: imageData) else { return }
             
             let beginImage = CIImage(image: inputImage)
-            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            for i in currentFilters {
+                i.setValue(beginImage, forKey: kCIInputImageKey)
+            }
             applyProcessing()
         }
     }
     
     func applyProcessing() {
-        let inputKeys = currentFilter.inputKeys
-
-        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
-
+        for filter in currentFilters {
+            let inputKeys = filter.inputKeys
+            
+            if inputKeys.contains(kCIInputIntensityKey) { filter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+            if inputKeys.contains(kCIInputRadiusKey) { filter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+            if inputKeys.contains(kCIInputScaleKey) { filter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        }
+        
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
         
