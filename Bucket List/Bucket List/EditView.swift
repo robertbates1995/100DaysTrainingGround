b4e67ad358867a562f316b4c40fab40a3cdd9632
Robert Bates <thegreatbobini@gmail.com>
2024-05-15 15:03:23 -0700

    added wikipediea request

--- Bucket List/Bucket List/EditView.swift
+++ Bucket List/Bucket List/EditView.swift
@@ -11,16 +11,13 @@ struct EditView: View {
     @Environment(\.dismiss) var dismiss
     @State private var loadingState = LoadingState.loading
     @State private var pages = [Page]()
-
+    @State private var name: String
+    @State private var description: String
     var location: Location
     var onSave: (Location) -> Void
     enum LoadingState {
         case loading, loaded, failed
     }
-
-
-    @State private var name: String
-    @State private var description: String
     
     init(location: Location, onSave: @escaping (Location) -> Void) {
         self.location = location
@@ -66,6 +63,29 @@ struct EditView: View {
                     dismiss()
                 }
             }
+        }
+    }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // we got some data back!
+            let items = try JSONDecoder().decode(Result.self, from: data)
+
+            // success – convert the array values to our pages array
+            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            loadingState = .loaded
+        } catch {
+            // if we're still here it means the request failed somehow
+            loadingState = .failed
         }
     }
 }
