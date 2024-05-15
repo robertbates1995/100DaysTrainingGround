2eb3360d9602814a98468fd57b0ff2f97d792258
Robert Bates <thegreatbobini@gmail.com>
2024-05-14 18:26:28 -0700

    adding Comparable conformance

--- Bucket List/Bucket List/ContentView.swift
+++ Bucket List/Bucket List/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ContentView: View {
     let users = [User(firstName: "First1", lastName: "Last1"),
                  User(firstName: "First2", lastName: "Last2"),
-                 User(firstName: "First3", lastName: "Last3")]
+                 User(firstName: "First3", lastName: "Last3")
+    ].sorted { $0.firstName > $1.lastName }
+    
     let values = [1, 5, 3, 6, 2, 9].sorted()
     
     var body: some View {
@@ -20,10 +22,14 @@ struct ContentView: View {
     }
 }
 
-struct User: Identifiable {
+struct User: Identifiable, Comparable {
     let id = UUID()
     var firstName: String
     var lastName: String
+    
+    static func < (lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.firstName
+    }
 }
 
 #Preview {
