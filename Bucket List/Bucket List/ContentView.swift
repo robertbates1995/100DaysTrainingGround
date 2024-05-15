7e37c21ecd9412f7ec4708f2fb6686bca1adcdfd
Robert Bates <thegreatbobini@gmail.com>
2024-05-15 11:50:28 -0700

    implementing authentication

--- Bucket List/Bucket List/ContentView.swift
+++ Bucket List/Bucket List/ContentView.swift
@@ -6,66 +6,29 @@
 //
 
 import SwiftUI
-import MapKit
+import LocalAuthentication
 
 struct ContentView: View {
-    @State private var position = MapCameraPosition.region(
-        MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-        )
-    )
-    
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
-    
     var body: some View {
-        Map(position: $position, interactionModes: [.rotate, .zoom]) {
-            ForEach(locations) { location in
-                Annotation(location.name, coordinate: location.coordinate) {
-                    Text(location.name)
-                        .font(.headline)
-                        .padding()
-                        .background(.blue)
-                        .foregroundStyle(.white)
-                        .clipShape(.capsule)
-                }
-                .annotationTitles(.hidden)
-            }
-        }
-        .mapStyle(.hybrid(elevation: .realistic))
-        .onMapCameraChange { context in
-            print(context.region)
-        }
         
-        HStack(spacing: 50) {
-            Button("Paris") {
-                position = MapCameraPosition.region(
-                    MKCoordinateRegion(
-                        center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                    )
-                )
-            }
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to authenticate using biometrics to unlock your data."
             
-            Button("Tokyo") {
-                position = MapCameraPosition.region(
-                    MKCoordinateRegion(
-                        center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                    )
-                )
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    //authenticated successfully
+                } else {
+                    //there was a problem
+                }
             }
         }
     }
-}
-
-struct Location: Identifiable {
-    let id = UUID()
-    var name: String
-    var coordinate: CLLocationCoordinate2D
 }
 
 #Preview {
