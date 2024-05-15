1578d8d50d8046e59442b240f46fa0687518dd25
Robert Bates <thegreatbobini@gmail.com>
2024-05-14 18:27:02 -0700

    altering names

--- Bucket List/Bucket List/ContentView.swift
+++ Bucket List/Bucket List/ContentView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    let users = [User(firstName: "First1", lastName: "Last1"),
-                 User(firstName: "First2", lastName: "Last2"),
-                 User(firstName: "First3", lastName: "Last3")
+    let users = [User(firstName: "Arnold", lastName: "Rimmer"),
+                 User(firstName: "Kristine", lastName: "Kochanski"),
+                 User(firstName: "David", lastName: "Lister"),
     ].sorted { $0.firstName > $1.lastName }
     
     let values = [1, 5, 3, 6, 2, 9].sorted()
