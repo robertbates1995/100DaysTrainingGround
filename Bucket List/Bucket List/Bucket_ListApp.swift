3378ebd8bc6a154f28581156420978918b7cae4a
Robert Bates <thegreatbobini@gmail.com>
2024-05-14 17:58:58 -0700

    creating bucket list

---
+++ Bucket List/Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Robert Bates on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
