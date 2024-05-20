db056941e039c07d57c527fbf129d8db7bb3d54e
Robert Bates <thegreatbobini@gmail.com>
2024-05-19 18:37:25 -0700

    allowed user to select photo from library

--- NameCard/NameCard/ContentView.swift
+++ NameCard/NameCard/ContentView.swift
@@ -5,31 +5,38 @@
 //  Created by Robert Bates on 5/18/24.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 import PhotosUI
 
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var selectedItem: PhotosPickerItem?
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", image: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
+                }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+            }
         }
-        .padding()
     }
     
     func loadImage() {
         Task {
             guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
             guard let inputImage = UIImage(data: imageData) else { return }
-            
-            let beginImage = CIImage(image: inputImage)
-            //currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-            //applyProcessing()
+            processedImage = Image(uiImage: inputImage)
         }
     }
 }
