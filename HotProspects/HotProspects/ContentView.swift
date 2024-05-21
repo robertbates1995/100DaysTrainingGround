168597e83b1ac3fbd7f0e1d4961930cc2d70cec2
Robert Bates <thegreatbobini@gmail.com>
2024-05-21 15:13:41 -0700

    implemented prospect view

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -9,16 +9,18 @@ import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    let possibleNumbers = 1...60
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.formatted()
-    }
     
     var body: some View {
-        Text(results)
+        TabView {
+            ProspectsView()
+                .tabItem { Label("Everyone", systemImage: "person.3") }
+            ProspectsView()
+                .tabItem { Label("Contacted", systemImage: "checkmark.circle") }
+            ProspectsView()
+                .tabItem { Label("Uncontacted", systemImage: "questionmark.diamond") }
+            MeView()
+                .tabItem { Label("Me", systemImage: "person.crop.square") }
+        }
     }
 }
 
