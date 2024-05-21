f43ee1160a90b589bd3ce33908d0c3ce61cc7deb
Robert Bates <thegreatbobini@gmail.com>
2024-05-21 09:28:55 -0700

    starting custom row

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -11,24 +11,11 @@ struct ContentView: View {
     @State private var backgroundColor = Color.red
     
     var body: some View {
-        VStack {
-            Text("Hello, World!")
-                .padding()
-                .background(backgroundColor)
-            
-            Text("Change Color")
-                .padding()
-                .contextMenu {
-                    Button("Red", systemImage: "checkmark.circle.fill", role: .destructive) {
-                        backgroundColor = .red
-                    }
-                    
-                    Button("Green") {
-                        backgroundColor = .green
-                    }
-                    
-                    Button("Blue") {
-                        backgroundColor = .blue
+        List {
+            Text("Taylor Swift")
+                .swipeActions {
+                    Button("Send Message") {
+                        print("hey")
                     }
                 }
         }
