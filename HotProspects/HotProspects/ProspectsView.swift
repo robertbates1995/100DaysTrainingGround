9413e2464f8f3ffece27e0a4c7e64bf5abbd76a0
Robert Bates <thegreatbobini@gmail.com>
2024-05-21 15:02:58 -0700

    adding new views to HotProspects

---
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,18 @@
+//
+//  Prospects.swift
+//  HotProspects
+//
+//  Created by Robert Bates on 5/21/24.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProspectsView()
+}
