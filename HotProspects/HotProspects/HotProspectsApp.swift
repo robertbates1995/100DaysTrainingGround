9a4e1d2c81e48ff44c328d0bbc4549e20fcdebda
Robert Bates <thegreatbobini@gmail.com>
2024-05-21 08:35:09 -0700

    creating HotProspects

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Robert Bates on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
