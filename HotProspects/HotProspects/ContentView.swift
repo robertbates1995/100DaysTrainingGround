34246aeb677929b9f9cc8c9a8cdb8855822c893c
Robert Bates <thegreatbobini@gmail.com>
2024-05-21 09:41:54 -0700

    adding notification

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -23,7 +23,16 @@ struct ContentView: View {
             }
 
             Button("Schedule Notification") {
-                // second
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                UNUserNotificationCenter.current().add(request)
             }
         }
     }
