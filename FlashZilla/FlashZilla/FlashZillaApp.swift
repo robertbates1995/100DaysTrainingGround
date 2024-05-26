d587b2856a0e06af41f22456d710eb45a66282f2
Robert Bates <thegreatbobini@gmail.com>
2024-05-26 13:58:22 -0700

    starting Flashzilla

---
+++ FlashZilla/FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Robert Bates on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
