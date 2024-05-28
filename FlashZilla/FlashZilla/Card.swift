f2a47ea0db0b0aac076c08e167ef1c0cf857f2a1
Robert Bates <thegreatbobini@gmail.com>
2024-05-28 12:10:57 -0700

    created Card struct

---
+++ FlashZilla/FlashZilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  FlashZilla
+//
+//  Created by Robert Bates on 5/28/24.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "example prompt", answer: "example answer")
+}
