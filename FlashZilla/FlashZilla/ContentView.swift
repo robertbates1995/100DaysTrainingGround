d11e4ffb539f684aaed17952001bcc4a0625ec1a
Robert Bates <thegreatbobini@gmail.com>
2024-05-28 12:38:05 -0700

    added card stack and background image

--- FlashZilla/FlashZilla/ContentView.swift
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -9,15 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
     var body: some View {
-        CardView(card: .example)
+        ZStack {
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
     }
 }
 
 extension View {
     func stacked(at position: Int, in total: Int) -> some View {
         let offset = Double(total - position)
-        return self.offset(y: offset * 10)
+        return self.offset(x: offset * 3, y: offset * -5)
     }
 }
 
