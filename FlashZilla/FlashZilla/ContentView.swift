a739973c2836b635629f31fb8922d8e6a6b249b4
Robert Bates <thegreatbobini@gmail.com>
2024-05-28 14:26:28 -0700

    added timer exit

--- FlashZilla/FlashZilla/ContentView.swift
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
     @State private var cards = Array<Card>(repeating: .example, count: 10)
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
@@ -31,11 +35,18 @@ struct ContentView: View {
                     }
                 }
                 Spacer()
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
             }
             if accessibilityDifferentiateWithoutColor {
                 VStack {
                     Spacer()
-
+                    
                     HStack {
                         Image(systemName: "xmark.circle")
                             .padding()
@@ -51,6 +62,20 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .padding()
                 }
+            }
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+            
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                isActive = true
+            } else {
+                isActive = false
             }
         }
     }
