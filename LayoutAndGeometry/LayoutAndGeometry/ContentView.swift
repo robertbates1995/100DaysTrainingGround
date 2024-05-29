11e7362fdc6e6d9fcb4b7d0262bb3a687cf892ae
Robert Bates <thegreatbobini@gmail.com>
2024-05-29 10:39:17 -0700

    position vs offset

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,38 +7,15 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twoblades")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                Image(.scissors)
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-
-            VStack {
-                Text("Full name:")
-                Text("Scissors McCutter")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                    .font(.largeTitle)
-            }
-        }
+        Text("hello world")
+            .position(x: 100, y: 100)
+            .background(.red)
+        Text("hello world")
+            .offset(x: 100, y: -100)
+            .background(.red)
+            .padding()
     }
 }
 
