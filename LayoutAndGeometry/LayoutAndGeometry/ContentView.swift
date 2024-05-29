9c019dac4573dd5d3f3b5badd17adc95a2ac611a
Robert Bates <thegreatbobini@gmail.com>
2024-05-29 10:12:23 -0700

    using alignment guide

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,15 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .lastTextBaseline) {
-            Text("Live")
-                .font(.caption)
-            Text("long")
-            Text("and")
-                .font(.title)
-            Text("prosper")
-                .font(.largeTitle)
+        VStack(alignment: .leading) {
+            Text("Hello, world!")
+                .alignmentGuide(.leading) { d in
+                    d[.leading]
+                }
+            Text("This is a longer line of text")
         }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
     }
 }
 
