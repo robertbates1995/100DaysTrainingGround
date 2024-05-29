093bc463145aa566d4a2d5c7d36a7a3fe00bbadb
Robert Bates <thegreatbobini@gmail.com>
2024-05-29 10:47:56 -0700

    using geometryReader to give relative size params

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader { proxy in
-            Image(.scissors)
-                .resizable()
-                .scaledToFit()
-                .frame(width: proxy.size.width * 0.8, height: 300)
+        HStack {
+            Text("IMPORTANT")
+                .frame(width: 200)
+                .background(.blue)
+            
+            GeometryReader { proxy in
+                Image(.scissors)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: proxy.size.width * 0.8)
+                    .frame(width: proxy.size.width, height: proxy.size.height)
+            }
         }
     }
 }
