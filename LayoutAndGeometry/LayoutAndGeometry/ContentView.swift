e280a103221139bdf90e6f5b529ffc4a44dfeb4e
Robert Bates <thegreatbobini@gmail.com>
2024-05-29 10:44:04 -0700

    using geometry reader

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("hello world")
-            .position(x: 100, y: 100)
-            .background(.red)
-        Text("hello world")
-            .offset(x: 100, y: -100)
-            .background(.red)
-            .padding()
+        GeometryReader { proxy in
+            Image(.scissors)
+                .resizable()
+                .scaledToFit()
+                .frame(width: proxy.size.width * 0.8, height: 300)
+        }
     }
 }
 
