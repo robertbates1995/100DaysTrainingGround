b52b24dfb3a3d3736ed227659ac21567424a5497
Robert Bates <thegreatbobini@gmail.com>
2024-05-30 14:27:44 -0700

    using filtering

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -9,16 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            allNames
+        } else {
+            allNames.filter { $0.localizedStandardContains(searchText) }
+        }
+    }
 
     var body: some View {
         NavigationStack {
-            Text("Searching for \(searchText)")
-                .searchable(text: $searchText, prompt: "Look for something")
-                .navigationTitle("Searching")
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
         }
     }
 }
-
 #Preview {
     ContentView()
 }
