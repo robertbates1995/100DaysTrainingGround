0aed90f8a894c9095db6cacbffe7b817701b54e4
Robert Bates <thegreatbobini@gmail.com>
2024-05-30 14:21:46 -0700

    creating alternative layouts

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,22 +7,33 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
-}
-
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
-    @State private var isShowingUser = false
+    @State private var layoutVertically = false
 
-    
     var body: some View {
-        Button("Tap Me") {
-            selectedUser = User()
+        Button {
+            layoutVertically.toggle()
+        } label: {
+            if layoutVertically {
+                VStack {
+                    UserView()
+                }
+            } else {
+                HStack {
+                    UserView()
+                }
+            }
         }
-        .sheet(item: $selectedUser) { user in
-            Text(user.id)
-                .presentationDetents([.medium, .large])
+    }
+    
+    struct UserView: View {
+        var body: some View {
+            Group {
+                Text("Name: Paul")
+                Text("Country: England")
+                Text("Pets: Luna and Arya")
+            }
+            .font(.title)
         }
     }
 }
