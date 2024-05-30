406b673200d626bf7c2f7e3f79085648ad8475f9
Robert Bates <thegreatbobini@gmail.com>
2024-05-30 14:07:15 -0700

    giving view priority

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationSplitView {
+        NavigationSplitView(columnVisibility: .constant(.all)) {
             NavigationLink("Primary") {
                 Text("New view")
             }
         } detail: {
             Text("Content")
+                .navigationTitle("Content View")
         }
+        .navigationSplitViewStyle(.balanced)
     }
 }
 
